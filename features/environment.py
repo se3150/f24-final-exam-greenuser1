1106b36cefa52a28cbfacbbb7033a53517be3888
d00483389 <d00483389@utahtech.edu>
2024-12-09 14:45:14 -0700

    driver

--- features/environment.py
+++ features/environment.py
@@ -1,16 +1,7 @@
 from selenium import webdriver
-import os 
-
-
 
 def before_all(context):
-    chromedriver_path = '/Users/marcoraromoreno/Desktop/chromedriver-mac-x64/chromedriver'
-    if not os.path.exists(chromedriver_path):
-        raise FileNotFoundError(f"Chromedriver not found at {chromedriver_path}")
-    options = webdriver.ChromeOptions()
-    options.headless = True
-    context.behave_driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
+    context.driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
 
 def after_all(context):
-    if hasattr(context, 'behave_driver') and context.behave_driver:
-        context.behave_driver.quit()
+    context.driver.quit()
