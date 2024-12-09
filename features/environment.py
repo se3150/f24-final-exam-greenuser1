9a7543a89b639b7b86612caa3049f66a8bd0bb52
d00483389 <d00483389@utahtech.edu>
2024-12-09 15:33:57 -0700

    P

--- features/environment.py
+++ features/environment.py
@@ -1,13 +1,12 @@
 from selenium import webdriver
 
 def before_all(context):
-    """Set up ChromeDriver session before tests."""
-    context.driver = webdriver.Chrome(
-        executable_path="/usr/local/bin/chromedriver",
-        options=webdriver.ChromeOptions().add_argument('--headless')
-    )
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--remote-debugging-port=9222")
+    context.driver = webdriver.Chrome(options=options)
 
 def after_all(context):
-    """Quit ChromeDriver session after tests."""
-    if context.driver:
-        context.driver.quit()
+    context.driver.quit()
