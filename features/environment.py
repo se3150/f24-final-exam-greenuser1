e1a6ed225e7762d5dc70c9d623f673ca8619f8ec
d00483389 <d00483389@utahtech.edu>
2024-12-09 14:14:06 -0700

    env

--- features/environment.py
+++ features/environment.py
@@ -1,14 +1,8 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
+from behave_webdriver import chrome 
 
 def before_all(context):
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-    context.behave_driver = webdriver.Chrome(options=chrome_options)
-    context.behave_driver.implicitly_wait(10)
-
+    context.behave_driver = Chrome(headless=True, timeout=10)
 
 def after_all(context):
-    context.behave_driver.quit()
+    if context.behave_driver:
+        context.behave_driver.quit()
