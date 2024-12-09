a76be23ace43ff3dc01c8242acd5e3a2001d44b0
d00483389 <d00483389@utahtech.edu>
2024-12-09 14:19:42 -0700

    env

--- features/environment.py
+++ features/environment.py
@@ -5,7 +5,7 @@ def before_all(context):
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
-    context.behave_driver = webdriver.Chrome(options=options, service=None)
+    context.behave_driver = webdriver.Chrome(options=options)
 
 
 def after_all(context):
